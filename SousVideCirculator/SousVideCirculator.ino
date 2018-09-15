cda0468a0eafaca51e31500d4f15fe51a7872594
Glauber Atanakados Santos <glauberatanaka@gmail.com>
2018-09-15 10:12:10 -0400

    Commit Inicial

---
+++ SousVideCirculator/SousVideCirculator.ino
@@ -0,0 +1,18 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+
+setar o teclado
+
+RTC - timer
+
+definir pinos e parâmetros
+
+modulos a serem desenvolvidos:
+    set temperatura
+    set timer
+    controlar relay em relação à temperatura
+    display
+
+
